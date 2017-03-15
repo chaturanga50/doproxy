0684b31289b513840c72dcf76a36b54683398458
Abeyrathna <UABEYC1@PEROOT.COM>
2017-03-15 12:14:24 +0530

    scale up and down

--- init.sh
+++ init.sh
@@ -6,6 +6,8 @@ chmod 600 ~/.ssh/id_rsa
 eval `ssh-agent -s`
 ssh-add ~/.ssh/id_rsa
 
+CONTENT="/usr/local/doproxy"
+INVENTORY="$CONTENT/inventory"
 STATIC_SERVER1="10.132.224.168"
 STATIC_SERVER1_USER="root"
 STATIC_SERVER1_CPU=$(ssh $STATIC_SERVER1_USER@$STATIC_SERVER1 'cat /tmp/CPU_LOAD')
@@ -14,6 +16,15 @@ STATIC_SERVER1_MEMORY=$(ssh $STATIC_SERVER1_USER@$STATIC_SERVER1 'cat /tmp/MEMOR
 CPU_THRESHOLD=80.00
 MEM_THRESHOLD=200
 
-if [ "$STATIC_SERVER1_CPU" -le "$CPU_THRESHOLD" ] && [ "$STATIC_SERVER1_MEMORY" -le "$MEM_THRESHOLD" ]
+if [ "$STATIC_SERVER1_CPU" -ge "$CPU_THRESHOLD" ] && [ "$STATIC_SERVER1_MEMORY" -le "$MEM_THRESHOLD" ];
     then
-    
+    cd $CONTENT; ruby doproxy.rb create
+    sleep 120
+fi
+
+if [ "$STATIC_SERVER1_CPU" -lt "$CPU_THRESHOLD" ] && [ "$STATIC_SERVER1_MEMORY" -gt "$MEM_THRESHOLD" ];
+    then
+    DELETE_SERVER_ID=$(tail -n1 $INVENTORY | awk '{ print $1}' | cut -d ')' -f1)
+    cd $CONTENT; ruby doproxy.rb delete $DELETE_SERVER_ID
+    sleep 120
+fi
